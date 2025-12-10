ce2cd491751d982e674d5c1d6476d3dd8c7cfced
baizi826 <1131546071@qq.com>
2025-12-10 20:08:59 +0800

    Revert "Del: Remove ship equipment change as lack of maintenance"
    This reverts commit fbb936f081bb4bbd5177a5d5addc757b5572cfea.

--- module/campaign/gems_farming.py
+++ module/campaign/gems_farming.py
@@ -120,8 +120,20 @@ class GemsFarming(CampaignRun, Dock, FleetEquipment, GemsEquipmentHandler):
         self.campaign.config.override(EnemyPriority_EnemyScaleBalanceWeight='S1_enemy_first')
 
     @property
+    def change_flagship(self):
+        return 'ship' in self.config.GemsFarming_ChangeFlagship
+
+    @property
+    def change_flagship_equip(self):
+        return 'equip' in self.config.GemsFarming_ChangeFlagship
+
+    @property
     def change_vanguard(self):
         return 'ship' in self.config.GemsFarming_ChangeVanguard
+
+    @property
+    def change_vanguard_equip(self):
+        return 'equip' in self.config.GemsFarming_ChangeVanguard
 
     @property
     def fleet_to_attack(self):
@@ -137,11 +149,30 @@ class GemsFarming(CampaignRun, Dock, FleetEquipment, GemsEquipmentHandler):
         Returns:
             bool: True if flagship changed.
         """
+
+        if self.config.GemsFarming_CommonCV == 'any':
+            index_list = range(3, 5)
+        else:
+            index_list = range(0, 5)
         logger.hr('Change flagship', level=1)
+        logger.attr('ChangeFlagship', self.config.GemsFarming_ChangeFlagship)
         self.fleet_enter(self.fleet_to_attack)
+        if self.change_flagship_equip:
+            logger.hr('Record flagship equipment', level=2)
+            self.fleet_enter_ship(FLEET_DETAIL_ENTER_FLAGSHIP)
+            self.ship_equipment_record_image(index_list=index_list)
+            self.ship_equipment_take_off()
+            self.fleet_back()
 
         logger.hr('Change flagship', level=2)
         success = self.flagship_change_execute()
+
+        if self.change_flagship_equip:
+            logger.hr('Equip flagship equipment', level=2)
+            self.fleet_enter_ship(FLEET_DETAIL_ENTER_FLAGSHIP)
+            self.ship_equipment_take_off()
+            self.ship_equipment_take_on_image(index_list=index_list)
+            self.fleet_back()
 
         return success
 
@@ -155,9 +186,22 @@ class GemsFarming(CampaignRun, Dock, FleetEquipment, GemsEquipmentHandler):
         logger.hr('Change vanguard', level=1)
         logger.attr('ChangeVanguard', self.config.GemsFarming_ChangeVanguard)
         self.fleet_enter(self.fleet_to_attack)
+        if self.change_vanguard_equip:
+            logger.hr('Record vanguard equipment', level=2)
+            self.fleet_enter_ship(FLEET_DETAIL_ENTER)
+            self.ship_equipment_record_image()
+            self.ship_equipment_take_off()
+            self.fleet_back()
 
         logger.hr('Change vanguard', level=2)
         success = self.vanguard_change_execute()
+
+        if self.change_vanguard_equip:
+            logger.hr('Equip vanguard equipment', level=2)
+            self.fleet_enter_ship(FLEET_DETAIL_ENTER)
+            self.ship_equipment_take_off()
+            self.ship_equipment_take_on_image()
+            self.fleet_back()
 
         return success
 
@@ -385,7 +429,7 @@ class GemsFarming(CampaignRun, Dock, FleetEquipment, GemsEquipmentHandler):
 
     def triggered_stop_condition(self, oil_check=True):
         # Lv32 limit
-        if self.campaign.config.LV32_TRIGGERED:
+        if self.change_flagship and self.campaign.config.LV32_TRIGGERED:
             self._trigger_lv32 = True
             logger.hr('TRIGGERED LV32 LIMIT')
             return True
@@ -405,7 +449,7 @@ class GemsFarming(CampaignRun, Dock, FleetEquipment, GemsEquipmentHandler):
             mode (str): `normal` or `hard`
             total (int):
         """
-        self.config.override(STOP_IF_REACH_LV32=True)
+        self.config.STOP_IF_REACH_LV32 = self.change_flagship
 
         while 1:
             self._trigger_lv32 = False
@@ -421,7 +465,9 @@ class GemsFarming(CampaignRun, Dock, FleetEquipment, GemsEquipmentHandler):
 
             # End
             if self._trigger_lv32 or self._trigger_emotion:
-                success = self.flagship_change()
+                success = True
+                if self.change_flagship:
+                    success = self.flagship_change()
                 if self.change_vanguard:
                     success = success and self.vanguard_change()
 
